59b3a4becab9c68854b7f6774771feb742a5c70a
sjlee <sj_lee@yahoo.com>
2017-12-17 03:41:27 -0500

    Restore https config file.

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ var config = function ()
     "0xa11d858c900487ad62dcc1cecdd87d4514fb70fa": "music.reidocoin.com.br"
   }
   
-  this.sslKey = "";
-  this.sslCertificate = "";
-  this.port = 3000;
+  this.sslKey = "C:/ProgramData/letsencrypt-win-simple/httpsacme-v01.api.letsencrypt.org/explorer.musicoin.org-key.pem";
+  this.sslCertificate = "C:/ProgramData/letsencrypt-win-simple/httpsacme-v01.api.letsencrypt.org/explorer.musicoin.org-crt.pem";
+  this.port = 443;
 }
 
 module.exports = config;
